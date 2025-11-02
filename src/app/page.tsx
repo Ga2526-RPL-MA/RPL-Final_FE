362e918249146cd1be72d0f6ba71e8bbc11640ab
myuga <muhyugabillawalrizqi@gmail.com>
2025-11-02 16:17:51 +0700

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,36 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div
+      className="relative h-screen w-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/home-bg.jpg')" }}
+    >      
+      <div className="absolute inset-0 bg-black/80"></div>
+  
+      <div className="relative z-10 flex items-start justify-start h-full pt-[100px] pl-[107px]">
+        <div className="bg-[#181D27] w-[600px] h-[369px] rounded-[45px]">
+          
+          <div className="flex items-center mt-[36px] ml-[34px]">
+            <div
+              className="w-[32px] h-[32px] rounded-[8px] bg-white bg-center bg-no-repeat bg-contain"
+              style={{ backgroundImage: "url('/logo.png')" }}
+            ></div>
+            <h1 className="text-white text-lg ml-3 font-[Montserrat] font-bold">RPL FINAL</h1>            
+          </div>
+          
+          <div className="relative item-center justify-center mt-[40px] ml-[70px] w-[456px] h-[183px]">
+            <h1 className="text-white w-[414] h-[120] font-bold text-4xl">Selamat Datang Di IFinal</h1>
+            <Link href="/auth/login">
+            <button className="bg-[#2E90FA] text-white font-bold rounded-[5px] w-[92] h-[46]">Masuk</button>
+            </Link>
+            <Link href="/auth/register">
+            <button className="border-1 rounded-[5px] font-bold text-white w-[173] h-[46] ml-5">Daftar Akun Baru</button>
+            </Link>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
